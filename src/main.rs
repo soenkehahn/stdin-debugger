5ea5785ea91731e280bf1590ce020ae791c7d662
Sönke Hahn <soenkehahn@gmail.com>
2021-06-23 13:51:06 -0400

    Detect closed stdin correctly

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,23 @@
-use std::io::{self, Read};
+use nix::poll::{poll, PollFd, PollFlags};
+use std::{
+    io::{self, Read},
+    thread,
+    time::Duration,
+};
+
+fn log(message: &str) {
+    eprintln!("[stdin-debugger]: {}", message);
+}
+
+fn stdin_is_closed() -> bool {
+    let mut poll_fds = [PollFd::new(0, PollFlags::all())];
+    poll(&mut poll_fds, 0).unwrap();
+    if let Some(events) = poll_fds[0].revents() {
+        events.contains(PollFlags::POLLHUP)
+    } else {
+        false
+    }
+}
 
 fn main() {
     loop {
@@ -6,13 +25,17 @@ fn main() {
         let length = io::stdin().read(&mut buffer).unwrap();
         match length {
             0 => {
-                eprintln!("stdin was closed");
-                break;
+                log("received 0 bytes");
             }
             length => {
                 let snippet = String::from_utf8_lossy(&buffer[..length]);
-                eprintln!("{} bytes read: {:?}", length, snippet);
+                log(&format!("{} bytes read: {:?}", length, snippet));
             }
         }
+        if stdin_is_closed() {
+            log("stdin is closed");
+            break;
+        }
+        thread::sleep(Duration::from_secs_f32(0.1));
     }
 }
